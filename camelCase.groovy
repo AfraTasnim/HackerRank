d877269a087158b31dcb1be30436761874f25fd8
Afra <afratasnimroza@gmail.com>
2022-09-25 11:56:08 +0600

    read input from console

--- camelCase.groovy
+++ camelCase.groovy
@@ -1,12 +1,19 @@
-def inputString= "thisIsTheInputString"
+/*https://www.hackerrank.com/challenges/camelcase/problem?isFullScreen=true*/
+
+println(" Enter Input:")
+
+def inputString= System.in.newReader().readLine()
+
+println(inputString)
+
 def getNumberOfWords(String inputString){
-    def numberOfWords=1
-    for(int i=1; i< inputString.length();i++){
+    def numberOfWords= 1
+    for(int i=1; i< inputString.length(); i++){
 
         if( Character.isUpperCase( inputString.charAt(i)) == true){
 
 
-            numberOfWords=numberOfWords+1
+            numberOfWords = numberOfWords + 1
 
         }
 
